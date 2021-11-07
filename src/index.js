e0e55885d7a58fcf8c9064a45f50ff177bcade6b
adrien-intern <adrien.degrange@aodocs.com>
2021-11-07 12:43:00 +0100

    restoring the grid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import $ from "jquery";
-$('#game-grid-body').text('Hello, IT!');
+let tableBody = $('#game-grid-body');
+for (let i = 0; i < 30; i++) {
+    let tr = '<tr>';
+    for (let j = 0; j < 30; j++) {
+        tr += '<td></td>';
+    }
+    tableBody.append(tr + '</tr>');
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import $ from "jquery"; 
 
-$('#game-grid-body').text('Hello, IT!'); 
+let tableBody: JQuery<HTMLElement> = $('#game-grid-body'); 
+
+for (let i:number = 0; i < 30; i++) {
+    let tr: string = '<tr>';
+
+    for (let j:number = 0; j < 30; j++) {
+        tr += '<td></td>'; 
+    }
+
+    tableBody.append(tr + '</tr>'); 
+}
 
