d5bccaa183493b7274cce6a820ee3f5011df356b
adrien-intern <adrien.degrange@aodocs.com>
2022-01-08 14:39:10 +0100

    cleansing

--- src/index.ts
+++ src/index.ts
@@ -33,23 +33,6 @@ deserializeFromJSON('{"liveCells":[{"x":-12,"y":-1},{"x":-11,"y":-1},{"x":-10,"y
     '{"x":-10,"y":-3},{"x":-11,"y":-2},{"x":-11,"y":0}]}', 
     ecosystem.livingCells);
 
-/*liveCells.hydrateSituationFromJSON('{"liveCells":[{"x":-4,"y":-9,"groupID":0},{"x":-3,"y":-9,"groupID":0},{"x":-2,"y":-9,"groupID":0},' + 
-    '{"x":-3,"y":-10,"groupID":0},{"x":-4,"y":-6,"groupID":1},{"x":-3,"y":-6,"groupID":1},{"x":-2,"y":-6,"groupID":1},{"x":-4,"y":-3,"groupID":2},' + 
-    '{"x":-4,"y":-4,"groupID":2},{"x":-2,"y":-4,"groupID":3},{"x":-2,"y":-3,"groupID":3},{"x":-4,"y":-1,"groupID":4},{"x":-3,"y":-1,"groupID":4},' + 
-    '{"x":-2,"y":-1,"groupID":4},{"x":-4,"y":2,"groupID":5},{"x":-3,"y":2,"groupID":5},{"x":-2,"y":2,"groupID":5},{"x":-3,"y":3,"groupID":5}]}'); 
-*/
-/*liveCells.hydrateSituationFromJSON(
-    '{"liveCells":[{"x":-5,"y":-2,"groupID":-1},{"x":-5,"y":-1,"groupID":-1},{"x":-5,"y":1,"groupID":-1},' 
-        + '{"x":-5,"y":0,"groupID":-1},{"x":-5,"y":2,"groupID":-1},{"x":-5,"y":3,"groupID":-1},{"x":-6,"y":-1,"groupID":-1},'
-        + '{"x":-6,"y":0,"groupID":-1},{"x":-6,"y":1,"groupID":-1},{"x":-6,"y":2,"groupID":-1}]}'); 
-*/
-
-// Glider 
-/*liveCells.hydrateSituationFromJSON(
-    '{"liveCells":[{"x":-12,"y":-14,"groupID":-1},{"x":-12,"y":-13,"groupID":-1},'
-        + '{"x":-12,"y":-12,"groupID":-1},{"x":-13,"y":-12,"groupID":-1},{"x":-14,"y":-13,"groupID":-1}]}'); 
-*/
-
 /** HERE WE GO! */
 updateUI(ecosystem.livingCells); 
 
